d2bb9d3e5960955dc3a5a9bd87f5c1315b0846e9
Juan Carlos Ruiz <juan.carlos.ruiz@janium.com>
2016-02-12 15:40:50 -0600

    Versión primera casi lista

---
+++ janium_recover.sh
@@ -0,0 +1,32 @@
+# janium_recover.sh
+#
+# Ejecuta comandos para que un repositorio quede listo para desarrollo
+
+# Crea directorios requeridos
+mkdir tmp
+mkdir log
+mkdir etc/apache
+mkdir logs
+mkdir logs/busquedas
+mkdir logs/ts
+mkdir htdocs/Descargar
+
+# Coloca los permisos requeridos a los directorios
+chmod 777 tmp
+chmod -R 777 logs
+chmod 777 htdocs/Descargar
+
+# Crea ligas simbólicas a los archivos y directorios requeridos por el sistema
+ln -s ~/JNMSDK/extrajnm/etc/logon.conf etc
+#ln -s ~/JNMSDK/extrajnm/etc/oai.conf etc
+#ln -s ~/JNMSDK/extrajnm/etc/sip2_daemon.conf etc
+#ln -s ~/JNMSDK/extrajnm/etc/plugin.conf etc
+#ln -s ~/JNMSDK/extrajnm/etc/z3950_daemon.conf etc
+ln -s ~/JNMSDK/extrajnm/etc/apache/* etc/apache
+
+ln -s ~/JNMSDK/extrajnm/htdocs/Imagenes/mylogos htdocs/Imagenes
+ln -s ~/JNMSDK/extrajnm/htdocs/Imagenes/referencista htdocs/Imagenes
+
+ln -s ~/JNMSDK/extrajnm/htdocs/autoregistro htdocs
+
+ln -s ~/JNMSDK/extrajnm/htdocs/js/custom/* htdocs/js/custom
